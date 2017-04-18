d08c10629e34bd7bb0b5bda7e421d711c4cdccac
モノセンス八木 <yagi@monosense.co.jp>
2017-04-18 13:51:48 +0900

    new

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+// SassとCssの保存先を指定
+gulp.task('sass', function () {
+    gulp.src('./sass/*.scss')
+        .pipe(sass({
+            outputStyle: 'expanded'
+        }))
+        .pipe(gulp.dest('./css/'));
+});
+
+//自動監視のタスクを作成(sass-watchと名付ける)
+gulp.task('sass-watch', ['sass'], function () {
+    var watcher = gulp.watch('./sass/*.scss', ['sass']);
+    watcher.on('change', function (event) {});
+});
+
+// タスク"task-watch"がgulpと入力しただけでdefaultで実行されるようになる
+gulp.task('default', ['sass-watch']);
